d15667fd44462bca1cdd2b8c1b22320844d8a7fa
theDavidUng <ung_david@yahoo.com>
2015-11-13 15:47:19 -0800

    spinning
    spinning to level 1

---
+++ Sphere/Assets/Scripts/MovingWall.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingWall : MonoBehaviour 
+{
+	public float speed;
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (0, 10, 0) * speed);
+	}
+}
