07c9ec08b239324640528d76a2832159bc5a4a83
zeroproduct <danrica92@yahoo.com>
2015-11-14 13:40:41 -0800

    Menu GUI and Level 1

---
+++ Sphere/Assets/Scripts/LevelScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelScript : MonoBehaviour {
+
+	public Button levelOne;
+	public Button levelTwo;
+	public Button levelThree;
+	public Button exitButton;
+	
+	void Start () {
+		levelOne = levelOne.GetComponent<Button> ();
+		levelTwo = levelTwo.GetComponent<Button> ();
+		levelThree = levelThree.GetComponent<Button> ();
+		exitButton = exitButton.GetComponent<Button> ();
+	}
+	
+	public void StartLevelOne() {
+		Application.LoadLevel (2);
+	}
+
+	public void StartLevelTwo() {
+		Application.LoadLevel (3);
+	}
+
+	public void StartLevelThree() {
+		Application.LoadLevel (4);
+	}
+
+	public void ExitGame() {
+		Application.Quit ();
+	}
+}
---
+++ Sphere/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public Button startText;
+	public Button exitText;
+	
+	void Start () {
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+	}
+
+	public void StartLevel() {
+		Application.LoadLevel (1);
+	}
+
+	public void ExitGame() {
+		Application.Quit ();
+	}
+
+}
