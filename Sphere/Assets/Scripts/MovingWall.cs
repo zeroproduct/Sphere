1204a9158ef1d9088eb3f5a0dc1b89e2340519f7
theDavidUng <ung_david@yahoo.com>
2015-11-13 17:42:57 -0800

    added moving objects
    level 2

--- Sphere/Assets/Scripts/MovingWall.cs
+++ Sphere/Assets/Scripts/MovingWall.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class MovingWall : MonoBehaviour 
 {
 	public float speed;
+	public float x;
+	public float y;
+	public float z;
 	// Update is called once per frame
 	void Update () 
 	{
-		transform.Rotate (new Vector3 (0, 10, 0) * speed);
+		transform.Rotate (new Vector3 (x, y, z) * speed);
 	}
 }
