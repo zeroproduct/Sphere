8343ad4c9d3d19fbaa81d43b53f2ac22a07742fc
DannyLy <nitialized@yahoo.com>
2015-12-09 00:59:56 -0800

    level 3 85% complete

---
+++ Sphere/Assets/Scripts/TileGenerator.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileGenerator : MonoBehaviour {
+
+	public GameObject currentTile;
+	public GameObject[] tilePrefabs;
+	private static TileGenerator instance;
+	private Stack<GameObject> leftTiles = new Stack<GameObject>();
+	private Stack<GameObject> topTiles = new Stack<GameObject>();
+
+	public Stack<GameObject> LeftTiles {
+		get { return leftTiles;}
+		set { leftTiles = value; }
+	}
+
+	public Stack<GameObject> TopTiles {
+		get { return topTiles;}
+		set { topTiles = value;}
+	}
+
+	public static TileGenerator Instance{
+		get
+		{
+			if(instance ==null){
+				instance = GameObject.FindObjectOfType<TileGenerator>();
+			}
+			return instance;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		createTile (100);
+
+		//create 50 tiles
+		for(int i=0; i<50; i++){
+			spawnTile();
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//create tiles to spawn later
+	public void createTile(int amt){
+		for (int i=0; i<amt; i++) {
+			leftTiles.Push(Instantiate(tilePrefabs[0]));
+			topTiles.Push(Instantiate(tilePrefabs[1]));
+			topTiles.Peek ().name = "TopTile";
+			topTiles.Peek ().SetActive(false);
+			leftTiles.Peek().name = "LeftTile";
+			leftTiles.Peek ().SetActive(false);
+
+		}
+	}
+
+	public void spawnTile(){
+		//if no tiles create tiles
+		if (leftTiles.Count == 0 || topTiles.Count == 0) {
+			createTile(10);
+		}
+		//generate random # between 0-1
+		int randomIndex = Random.Range (0, 2);
+		if (randomIndex == 0) {
+			GameObject temp = leftTiles.Pop ();
+			temp.SetActive(true);
+			temp.transform.position = currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position;
+			currentTile = temp;
+		} else if(randomIndex==1) {
+			GameObject temp = leftTiles.Pop ();
+			temp.SetActive(true);
+			temp.transform.position = currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position;
+			currentTile = temp;		
+		}
+
+		currentTile= (GameObject)Instantiate(tilePrefabs[randomIndex], currentTile.transform.GetChild(0).transform.transform.GetChild(randomIndex).position, Quaternion.identity);
+	}
+}
---
+++ Sphere/Assets/Scripts/TileScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileScript : MonoBehaviour {
+
+	private float fallDelay = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.tag == "Player") {
+			TileGenerator.Instance.spawnTile();
+			StartCoroutine(fallDown());
+		}
+	}
+
+	//co routine to fall down after 2sec of crossing
+	IEnumerator fallDown() {
+		yield return new WaitForSeconds (fallDelay);
+		GetComponent<Rigidbody> ().isKinematic = false;
+		yield return new WaitForSeconds(1);
+		switch (gameObject.name) {
+
+		case "LeftTile":
+			TileGenerator.Instance.LeftTiles.Push(gameObject);
+			gameObject.GetComponent<Rigidbody>().isKinematic = true;
+			gameObject.SetActive(false);
+				break;
+
+		case "TopTile":
+			TileGenerator.Instance.TopTiles.Push(gameObject);
+			gameObject.GetComponent<Rigidbody>().isKinematic = true;
+			gameObject.SetActive(false);
+			break;
+
+		}
+	}
+}
